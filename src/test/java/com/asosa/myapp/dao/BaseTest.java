66bbee95ffecb1928e97e1806de8dca0ce420e04
U-HYDROGEN\asosa <asosa01@gmail.com>
2009-11-08 22:54:27 +0900

    added transactionManager bean

--- src/test/java/com/asosa/myapp/dao/BaseTest.java
+++ src/test/java/com/asosa/myapp/dao/BaseTest.java
@@ -2,10 +2,11 @@ package com.asosa.myapp.dao;
 
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/applicationContext.xml"})
-public class BaseTest {
+public abstract class BaseTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 }
