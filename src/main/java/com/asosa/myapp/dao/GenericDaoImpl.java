d93f0268589a336ef2db8c69103acef1839be77b
U-HYDROGEN\asosa <asosa01@gmail.com>
2009-11-14 23:26:16 +0900

    autowired sessionfactory

--- src/main/java/com/asosa/myapp/dao/GenericDaoImpl.java
+++ src/main/java/com/asosa/myapp/dao/GenericDaoImpl.java
@@ -3,14 +3,26 @@ package com.asosa.myapp.dao;
 import java.io.Serializable;
 import java.util.List;
 
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 
-public abstract class GenericDaoImpl<T, PK extends Serializable> extends HibernateDaoSupport implements GenericDao<T, PK> {
+public abstract class GenericDaoImpl<T, PK extends Serializable> implements GenericDao<T, PK> {
 	private final Class<T> type;
+	private HibernateTemplate hibernateTemplate;
 
 	public GenericDaoImpl(Class<T> type) {
 		this.type = type;
 	}
+	
+	@Autowired(required=true)
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.hibernateTemplate = new HibernateTemplate(sessionFactory);
+    }
+	
+    public HibernateTemplate getHibernateTemplate() {
+        return hibernateTemplate;
+    }
 
 	@SuppressWarnings("unchecked")
 	public T find(PK id) {
